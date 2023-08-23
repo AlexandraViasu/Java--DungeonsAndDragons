f81bcbdbb2ef2ec0abcd667a0f5553bdb0e49968
AlexandraViasu <viasu.alexandra@gmail.com>
2023-08-23 15:24:15 +0100

    eldritch blast done

--- src/main/java/Adventurers/Nukers/Spells/EldritchBlast.java
+++ src/main/java/Adventurers/Nukers/Spells/EldritchBlast.java
@@ -2,23 +2,18 @@ package Adventurers.Nukers.Spells;
 
 import Enemies.Enemy;
 
-
-public class FireBall implements ISpell {
-
+public class EldritchBlast implements ISpell{
     private final int damage;
 
 
-    public FireBall(int damage){
-        this.damage = 30;
+    public EldritchBlast(){
+        this.damage = 40;
     }
 
     public int getDamage() {
         return damage;
-
     }
-
-    public void cast(Enemy enemy){
+    public void cast(Enemy enemy) {
         enemy.takeDamage(damage);
     }
-
 }
